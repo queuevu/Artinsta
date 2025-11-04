a9e9850b126578ddb230175cfb660d629ae8e092
queuevu <viyolahezeldsouza@gmail.com>
2025-11-04 10:51:41 +0530

    corrected a small mistake

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.get("/upload",function(req,res){
     
-    res.sendFile(path.join(__dirname, 'public', 'home.html'));
+    res.sendFile(path.join(__dirname, 'public', 'upload.html'));
 })
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'home.html'));
